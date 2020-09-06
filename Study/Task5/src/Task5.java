8db5258d19da3acba0188ff9d0e5158abe561864
zlobr <zlobriy@yandex.ru>
2020-09-07 01:19:00 +0300

    Добавил сортировку, в будущем пригодится. :)

---
+++ Study/Task5/src/Task5.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Task5 {
+    /*5. Ввести три целых числа с консоли. Вывести на
+    экран наименьшее из них.*/
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] numbers = new int[3];
+
+        for (int i = 0; i < numbers.length; i++)
+        {
+            String s = reader.readLine();
+            numbers[i] = Integer.parseInt(s);
+        }
+        printMin(numbers);
+
+    }
+
+    public static void printMin(int[] arr) {
+        for (int min = 0; min < arr.length; min++) {
+            int least = min;
+            for (int j = min + 1; j < arr.length; j++) {
+                if (arr[j] < arr[least]) {
+                    least = j;
+                }
+            }
+            int tmp = arr[min];
+            arr[min] = arr[least];
+            arr[least] = tmp;
+        }
+            System.out.println(arr[0]);
+
+    }
+
+
+
+}
