599ae70f55049e174c866fe1fdd77e467b0ed7c4
Zlobriy <zlobriy@yandex.ru>
2020-11-28 01:19:14 +0300

    я ебу алибабу

---
+++ JavaCore/homework/src/L3/task5/Boat.java
@@ -0,0 +1,43 @@
+package L3.task5;
+
+public class Boat implements Runnable {
+    Object lock;
+    private int count;
+    private int number;
+
+    public Boat(Object lock, int count) {
+
+        this.lock = lock;
+        this.count = count;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            number = i;
+            takeBoat(number);
+            count--;
+            if (count == 0){
+
+            }
+            if (i == 4) {
+                i = 0;
+            }
+
+        }
+    }
+
+    public void takeBoat(int number) {
+        synchronized (lock) {
+            int random = (int) (200 + Math.random() * 800);
+            System.out.println("Boat number " + this.number + " is sailing " + random + " time");
+            try {
+                Thread.sleep(random);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Boat number " + this.number + " is finish sail");
+            lock.notify();
+        }
+    }
+}
